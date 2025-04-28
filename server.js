6eb63d4670fd1681a7d5d56c64df8aeb3d6d196a
Theophilus <theondukwe@gmail.com>
2025-04-28 14:19:23 -0400

    completed project with debugging and fixed the thumbup/down functionality

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+const url = "mongodb+srv://theondukwe:discursive1@cluster0.vhrw8jw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const dbName = "demo";
 
 app.listen(3000, () => {
@@ -38,11 +38,25 @@ app.post('/messages', (req, res) => {
   })
 })
 
-app.put('/messages', (req, res) => {
+app.put('/thumbUp', (req, res) => {
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
       thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+app.put('/thumbdown', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp - 1
     }
   }, {
     sort: {_id: -1},
